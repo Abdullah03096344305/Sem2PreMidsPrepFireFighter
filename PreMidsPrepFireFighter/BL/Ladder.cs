fad6fd935bfdf707b6cc12f45d9971e7ce6b5b50
Abdullah03096344305 <abdullah03096344305@gmail.com>
2023-06-02 09:50:51 +0500

    Sem2PreMidsPrepFireFighter

---
+++ PreMidsPrepFireFighter/BL/Ladder.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PreMidsPrepFireFighter.BL
+{
+    class Ladder
+    {
+        private int length;
+        private string color;
+        public Ladder(int length, string color)
+        {
+            this.length = length;
+            this.color = color;
+        }
+
+    }
+    class HosePipe
+    {
+        private string material;
+        private string shape;
+        private float diameter;
+        private float flowrate;
+
+        public HosePipe(string material, string shape, float diameter, float flowrate)
+        {
+            this.material = material;
+            this.shape = shape;
+            this.diameter = diameter;
+            this.flowrate = flowrate;
+        }
+    }
+
+    class FireFighter
+    {
+        private string name;
+        public FireFighter(string name)
+        {
+            this.name = name;
+        }
+        public string GetName()
+        {
+            return name;
+        }
+        public void Drive()
+        {
+            Console.WriteLine(name + "  is Driving the Truck");
+        }
+        public void ExtinguishFire()
+        {
+            Console.WriteLine(name + "  is Extinguishing the Fire");
+        }
+        
+    }
+    class FireChief : FireFighter
+    {
+        public FireChief(string name) : base(name)
+        {           
+        }
+        public void DelegateResponsibility(string FireFighterName)
+        {
+            Console.WriteLine("Tell " + FireFighterName + " to Extinguish Fire");
+        }
+    }
+    class FireTruck
+    {
+        private Ladder li;
+        private HosePipe h1;
+        private FireFighter driver;
+        public FireTruck(HosePipe h1,FireFighter driver)
+        {
+            li = new Ladder(34, "Black");
+            this.h1 = h1;
+            this.driver = driver;
+        }
+    }
+}
---
+++ PreMidsPrepFireFighter/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using PreMidsPrepFireFighter.BL;
+using System.Threading.Tasks;
+
+namespace PreMidsPrepFireFighter
+{
+    class Program
+    {
+        static List<FireFighter> firefighters = new List<FireFighter>();
+        static List<FireTruck> fireTrucks = new List<FireTruck>();
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press 1 to Add a FireFighter");
+            Console.WriteLine("Press 2 to Add a Firetruck");
+            int option = int.Parse(Console.ReadLine());            
+                if (option == 1)
+                {
+                    Console.WriteLine("Enter FireFighter Name: ");
+                    string name = Console.ReadLine();
+                    firefighters.Add(new FireFighter(name));
+                    Console.WriteLine("FireFighter Name Is " + name);
+
+
+                }
+                else if (option == 2)
+                {
+                    for (int i = 0; i < firefighters.Count; i++)
+                    {
+                        Console.WriteLine("FireFighter Name Is " + firefighters[i].GetName());
+                    }
+                    Console.Write("Choose a firefighter by number: ");
+                    int firefighterIndex = int.Parse(Console.ReadLine());
+                    firefighterIndex--;
+                    if (firefighterIndex >= 0 && firefighterIndex < firefighters.Count)
+                    {
+                        Console.WriteLine("Fire truck added successfully!");
+                        fireTrucks.Add(new FireTruck(new HosePipe("Hard_Plastic", "Ovel", 1.0f, 2.0f), firefighters[firefighterIndex]));
+                    }
+                }
+            
+            Console.ReadKey();
+        }
+    }
+}
